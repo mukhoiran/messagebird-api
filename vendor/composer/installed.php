019ed1960f927d39eaf79a6633277059b773c91a
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-10-11 20:29:08 +0700

    install messagebird dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b1d6c8f22c0ed336fb5ceb9792dc1ca08a33d285',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b1d6c8f22c0ed336fb5ceb9792dc1ca08a33d285',
+            'dev_requirement' => false,
+        ),
+        'messagebird/php-rest-api' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../messagebird/php-rest-api',
+            'aliases' => array(),
+            'reference' => '128c781855638f3fe42e346f4df4dc165378d3c0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
